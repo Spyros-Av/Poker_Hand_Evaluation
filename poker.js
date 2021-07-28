477f901c0b354e0148cb60306e127cee234b0c0e
Spyros Avramiotis <spyros7msn@hotmail.com>
2021-07-28 10:57:44 +0300

    fix return type at evaluate function

--- poker.js
+++ poker.js
@@ -1,39 +1,38 @@
 const hand = [
   {
-    rank: '9',
+    rank: '10',
     suit: 'diamonds',
   },
   {
-    rank: '9',
+    rank: 'q',
     suit: 'diamonds',
   },
   {
-    rank: '3',
+    rank: 'q',
     suit: 'diamonds',
   },
   {
-    rank: '9',
+    rank: 'q',
     suit: 'diamonds',
   },
   {
-    rank: '9',
+    rank: 'Q',
     suit: 'diamonds',
   },
 
 ];
 
-
-
 const handType = {
-  STRAIGHT_FLUSH: 10,
-  FOUR_OF_A_KIND: 9,
-  FOUL_HOUSE: 8,
-  FLUSH: 7,
-  STRAIGHT: 6,
-  THREE_OF_A_KIND: 5,
-  TWO_PAIRS: 4,
-  ONE_PAIR: 3,
-  HIGH_CARD: 2,
+  ROYAL_FLUSH: 10,
+  STRAIGHT_FLUSH: 9,
+  FOUR_OF_A_KIND: 8,
+  FOUL_HOUSE: 7,
+  FLUSH: 6,
+  STRAIGHT: 5,
+  THREE_OF_A_KIND: 4,
+  TWO_PAIRS: 3,
+  ONE_PAIR: 2,
+  HIGH_CARD: 1,
 };
 
 const groupedHand = (hand) => hand.reduce(
@@ -56,6 +55,7 @@ const getValues = (groupedHand, maped) => {
 const suit = hand.map(item => item.suit);
 // console.log('----Suit----')
 // console.log(suit);
+
 const rank = hand.map(item => item.rank);
 for (let i = 0; i < rank.length; i++) {
   (rank[i] === 'J' || rank[i] === 'j') ? rank[i] = 11 :
@@ -63,16 +63,16 @@ for (let i = 0; i < rank.length; i++) {
       (rank[i] === 'K' || rank[i] === 'k') ? rank[i] = 13 :
         (rank[i] === 'A' || rank[i] === 'a') ? rank[i] = 14 : false;
 };
-
 // console.log('----Rank----')
 // console.log(rank);
+
 const rankHand = getValues(groupedHand, rank);
 // console.log('----RankHand----')
 // console.log(rankHand);
+
 const suitHand = getValues(groupedHand, suit);
 // console.log('----SuitHand----');
 // console.log(suitHand);
-
 
 const sorted = rank.sort((a, b) => a - b);
 const isSorted = [];
@@ -87,49 +87,76 @@ for (let i = 0; i < sorted.length - 1; i++) {
 const highCard = (rank) => {
   let highestCard = 0;
   for (let elem of rank) {
-    highestCard < elem ? highestCard = elem : highestCard;
+    highestCard < elem ? highestCard = Number(elem) : highestCard;
   }
   return highestCard;
 };
 
+const PlayerHand = function (handType, strength) {
+  this.hand = handType;
+  this.highCard = strength;
+}
+
 function evaluate(hand) {
-  if (suitHand.find(num => num === 5) && isSorted.every(num => num === -1) && isSorted.length === 4) {
+  const strength = highCard(rank);
+  if (suitHand.find(num => num === 5) && isSorted.every(num => num === -1) && isSorted.length === 4 && strength === 14) {
+    console.log('Your hand is Royal Flush');
+    const player = new PlayerHand(handType.ROYAL_FLUSH, strength);
+    return player;
+
+  } else if (suitHand.find(num => num === 5) && isSorted.every(num => num === -1) && isSorted.length === 4) {
     console.log('Your hand is Straight Flush');
-    return [handType.STRAIGHT_FLUSH, highCard(rank)];
+    const player = new PlayerHand(handType.STRAIGHT_FLUSH, strength);
+    return player;
 
   } else if (rankHand.find(num => num === 4)) {
     console.log('Your hand is Four of A Kind')
-    return [handType.FOUR_OF_A_KIND, highCard(rank)];
+    const player = new PlayerHand(handType.FOUR_OF_A_KIND, strength);
+    return player;
 
   } else if (rankHand.find(num => num === 3) && rankHand.find(num => num === 2)) {
-    console.log('Your hand is Full House')
-    return [handType.FOUL_HOUSE, highCard(rank)];
+    console.log('Your hand is Full House');
+    const player = new PlayerHand(handType.FOUL_HOUSE, strength);
+    return player;
 
   } else if (suitHand.find(num => num === 5)) {
-    console.log('Your hand is Flush')
-    return [handType.FLUSH, highCard(rank)];
+    console.log('Your hand is Flush');
+    const player = new PlayerHand(handType.FLUSH, strength)
+    return player;
 
   } else if (isSorted.every(num => num === -1) && isSorted.length === 4) {
     console.log('Your hand is Straight');
-    return [handType.STRAIGHT, highCard(rank)];
+    const player = new PlayerHand(handType.STRAIGHT, strength);
+    return player;
 
   } else if (rankHand.find(num => num === 3)) {
-    console.log('Your hand is Three of A Kind')
-    return [handType.THREE_OF_A_KIND, highCard(rank)];
+    console.log('Your hand is Three of A Kind');
+    const player = new PlayerHand(handType.THREE_OF_A_KIND, strength);
+    return player;
 
   } else if (rankHand.find(num => num === 2) && rankHand.length === 3) {
-    console.log('Your hand is Two Pairs')
-    return [handType.TWO_PAIRS, highCard(rank)];
-    
+    console.log('Your hand is Two Pairs');
+    const player = new PlayerHand(handType.TWO_PAIRS, strength);
+    return player;
+
   } else if (rankHand.find(num => num === 2)) {
-    console.log('Your hand is One Pair')
-    return [handType.ONE_PAIR, highCard(rank)];
+    console.log('Your hand is One Pair');
+    const player = new PlayerHand(handType.ONE_PAIR, strength);
+    return player;
 
   } else {
-    console.log('Your hand is High Card')
-    return [handType.HIGH_CARD, highCard(rank)];;
+    console.log('Your hand is High Card');
+    const player = new PlayerHand(handType.HIGH_CARD, strength);
+    return player;
   }
 };
 
-const playerHand = evaluate(hand);
-console.log(playerHand);
+
+const player1 = evaluate(hand);
+console.log(player1);
+
+
+
+
+
+
